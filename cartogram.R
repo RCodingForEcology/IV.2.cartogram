a333fef090642f0fe32cb8e406890071315cb088
Duccio Rocchini <ducciorocchini@gmail.com>
2025-03-31 12:22:54 +0200

    Update cartogram.R

--- cartogram.R
+++ cartogram.R
@@ -18,41 +18,43 @@ PresAbs.bias <- st_transform(PresAbs.bias, "EPSG:8857")
 PresAbs.bias <- st_join(PresAbs.bias, World.ea["iso_a3"])
 
 count.PresAbs.bias <- PresAbs.bias |>
-    st_drop_geometry() |>
-    group_by(iso_a3) |>
-    summarize(countISO = sum(Observed))
+  st_drop_geometry() |>
+  group_by(iso_a3) |>
+  summarize(countISO = sum(Observed))
 
 World.bias <- World.ea |>
-    left_join(count.PresAbs.bias, by = "iso_a3") |>
-    mutate(countISO = ifelse(is.na(countISO), 0, countISO))
+  left_join(count.PresAbs.bias, by = "iso_a3") |>
+  mutate(countISO = ifelse(is.na(countISO), 0, countISO))
 
 cart.cont <- cartogram_cont(World.bias, "countISO", itermax = 30, threshold = 0.16, verbose = FALSE)
 
 map.standard <- tm_shape(World.bias) +
-    tm_polygons("countISO", style = "jenks", palette = "cividis") +
-    tm_layout(frame = FALSE, legend.position = c("left", "bottom"),
-              legend.width = 1.5, legend.outside = TRUE)
+  tm_polygons("countISO", fill.scale = tm_scale_intervals(style="jenks", values = "cividis")) +
+  tm_layout(frame = FALSE, legend.position = c("left", "bottom"),
+            legend.width = 9, legend.outside = TRUE)
+
 map.carto1 <- tm_shape(cart.cont) +
-    tm_polygons("countISO", style = "jenks", palette = "cividis") +
-    tm_layout(frame = FALSE, legend.position = c("left", "bottom"),
-              legend.width = 1.5, legend.outside = TRUE)
+  tm_polygons("countISO", fill.scale = tm_scale_intervals(style="jenks", values = "cividis")) +
+  tm_layout(frame = FALSE, legend.position = c("left", "bottom"),
+            legend.width = 9, legend.outside = TRUE)
+
 tmap_arrange(map.standard, map.carto1)
 
 cart.ncont <- cartogram_ncont(World.bias, weight = "countISO", k = 1)
 
 tm_shape(World.ea) +
-    tm_borders() +
-    tm_shape(cart.ncont) +
-    tm_polygons("countISO", style = "jenks", palette = "cividis") +
-    tm_layout(frame = FALSE, legend.position = c("left", "bottom"),
-              legend.width = 1.5, legend.outside = TRUE)
+  tm_borders() +
+  tm_shape(cart.ncont) +
+  tm_polygons("countISO", fill.scale = tm_scale_intervals(style="jenks", values = "cividis")) +
+  tm_layout(frame = FALSE, legend.position = c("left", "bottom"),
+            legend.width = 9, legend.outside = TRUE)
+
 
 cart.dorling <- cartogram_dorling(World.bias, "countISO")
 
 tm_shape(World.ea) +
-    tm_borders() +
-    tm_shape(cart.dorling) +
-    tm_polygons("countISO", style = "jenks", palette = "cividis") +
-    tm_layout(frame = FALSE, legend.position = c("left", "bottom"),
-              legend.width = 1.5, legend.outside = TRUE)
-
+  tm_borders() +
+  tm_shape(cart.dorling) +
+  tm_polygons("countISO", fill.scale = tm_scale_intervals(style="jenks", values = "cividis")) +
+  tm_layout(frame = FALSE, legend.position = c("left", "bottom"),
+            legend.width = 9, legend.outside = TRUE)
